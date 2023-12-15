7b00109ef49e6835090e58fae810667f1c150bdf
takeover-da <dlslyn@naver.com>
2023-12-15 14:05:57 +0900

    주석제거

--- Cal.java
+++ Cal.java
@@ -2,7 +2,6 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        //New Work
 if ( (number1 < 0 or number2 < 0)  && (number1 + number2 < 0) )
                                 return;
                         return number1+number2;
