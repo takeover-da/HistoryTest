1a2cc9d8225d03498bce4ee5f2137045ae465c76
takeover-da <dlslyn@naver.com>
2023-12-15 14:31:05 +0900

    내용수정

--- Cal.java
+++ Cal.java
@@ -1,6 +1,12 @@
 public class Calculator {
-     public static void main(String[] args) {
-           //New Work
-     }
+    public static void main(String[] args) {
+	    //New Work
+        
+    if ( (number1 < 0 or number2 < 0)  && (number1 + number2 < 0) )
+	    return;
+    return number1+number2;
 }
 
+
+    }
+}
