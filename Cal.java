e5a7f02acce484dbce18821eeadf03fa613b83c4
takeover-da <dlslyn@naver.com>
2023-12-15 14:19:09 +0900

    주석 추가 버전으로 복원

--- Cal.java
+++ Cal.java
@@ -1,12 +1,6 @@
-import java.util.Scanner;
-
 public class Calculator {
-    public static void main(String[] args) {
-if ( (number1 < 0 or number2 < 0)  && (number1 + number2 < 0) )
-                                return;
-                        return number1+number2;
+     public static void main(String[] args) {
+           //New Work
+     }
 }
 
-
-    }
-}
